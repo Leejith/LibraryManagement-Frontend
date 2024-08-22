aee154360eb51cc15f680fdd2ad01858fa30d370
jeswin-p <jeswin23231@gmail.com>
2024-08-22 11:04:46 +0530

    staff completed

--- src/Components/Staff/StaffLogin.js
+++ src/Components/Staff/StaffLogin.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import '../../Assets/Styles/StaffLogin.css'
 
 function StaffLogin() {
   return (
-    <div>StaffLogin</div>
+    <div>StaffLogin
+    <form className=''>
+        <h1>Staff Login</h1>
+        <input type='email' placeholder='Enter E-mail'/>
+        <input type='password' placeholder='Enter Password'/><br/>
+        Forget Password<br/>
+        <button>Login</button>
+        <button>Register</button>
+      </form>
+    </div>
   )
 }
 
