a0598a3429e288459acf6fb4e4c97bb93cdc6efd
jeswin-p <jeswin23231@gmail.com>
2024-08-24 09:59:28 +0530

    navbar

---
+++ src/Components/common/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+              BOOKWORLD
+          </a>
+        </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
