8fefd4b3bf3aa1098d3007dc9c77bca70325e189
Leejith <leejith2002@gmail.com>
2024-08-26 16:18:51 +0100

    reg completed

---
+++ src/Components/Students/StudentReg.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../../Assets/Styles/StudentReg.css'
+
+function StaffRegister() {
+  return (
+    <div class="register1">
+        <div class="form1">
+        <h1>REGISTER FORM</h1>
+        <input type='text' placeholder='NAME' class='reg-name1'/>
+        <input type='text' placeholder='DEPARTMENT' class='reg-dep1'/>
+        <input type='number' placeholder='REGISTER NUMBER' class='reg-no1'/>
+        <input type='email' placeholder='Enter E-mail' class='reg-email1'/>
+        <input type='password' placeholder='Enter Password' class='reg-password1'/>
+        <button type='button' class="reg-btn1">REGISTER</button>
+        </div>
+        <div class='image0'>
+        <img src="" alt="" class="img0" /> 
+        </div>
+        </div>
+  )
+}
+
+export default StaffRegister
